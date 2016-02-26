54a9347e65e5081a967696b6eb3c4a3cc123b896
vova.mukovozov.sergata <vova.mukovozov@sergata.com>
2016-02-26 17:50:22 +0200

    added notes migration

--- migrations/20160220195041_notes.js
+++ migrations/20160220195041_notes.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex, Promise) {
-  
+
+  return knex.schema.createTable('notes', function (table) {
+      table.increments().primary()
+
+      //id
+      table.integer('user_id');
+
+      // Note details
+      table.string('title', 35).notNull();
+      table.string('description', 200).notNull();
+      table.boolean('importance').defaultTo(false);
+      table.boolean('is_active').defaultTo(true);
+
+      // Other
+      table.timestamps(); // created_at / updated_at
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('notes');
 };
