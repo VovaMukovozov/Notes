1f2212d8bb078c982df572853dd141082906c29a
vova.mukovozov.sergata <vova.mukovozov@sergata.com>
2016-02-26 17:54:53 +0200

    added user routes

--- routes/users.js
+++ routes/users.js
@@ -7,17 +7,19 @@ var express = require('express'),
 
 // GET
 router.get('/',  users.list);
+router.get('/:id',users.single);
 
 // POST
-//router.post('/', users.register.registration);
+router.post('/', users.create);
 
 // PUT
-//router.put('/confirm/email', users.profile.confirmEmail);
+router.put('/:id', users.update);
 //router.put('/password/forgot', users.password.forgot);
 //router.put('/password', users.password.change);
 //router.put('/profile', auth.allowd(), users.profile.update);
 
 // DELETE
+router.delete('/:id', users.delete);
 
 
 module.exports = router;
